7099688297455b43dae93b9bdd2d971c3011e271
floop <ifloop@ya.ru>
2022-05-30 22:10:17 +0500

    few tests

---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,84 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Dakak-Takto/yandex-practicum-go-diploma/internal/entity"
+	"github.com/Dakak-Takto/yandex-practicum-go-diploma/internal/utils"
+	"github.com/Dakak-Takto/yandex-practicum-go-diploma/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/sessions"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_handlers_userRegister(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mocks.NewMockService(ctrl)
+
+	testUser := entity.User{
+		ID:       24,
+		Login:    "volodya",
+		Password: utils.Hash("secret"),
+		Balance:  2022,
+	}
+
+	m.EXPECT().GetUserByLogin(testUser.Login).Return(nil, nil)
+	m.EXPECT().RegisterUser(testUser.Login, testUser.Password).Return(&testUser, nil)
+
+	h := handler{
+		service:  m,
+		sessions: sessions.NewCookieStore([]byte("secret")),
+	}
+	handler := http.HandlerFunc(h.userRegister)
+
+	b, _ := json.Marshal(map[string]string{
+		"login":    testUser.Login,
+		"password": testUser.Password,
+	})
+	req, _ := http.NewRequest(http.MethodPost, "", bytes.NewReader(b))
+
+	recorder := httptest.NewRecorder()
+	handler.ServeHTTP(recorder, req)
+
+	require.Equal(t, recorder.Code, http.StatusOK)
+}
+
+func Test_handlers_userLogin(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mocks.NewMockService(ctrl)
+
+	testUser := entity.User{
+		ID:       24,
+		Login:    "volodya",
+		Password: utils.Hash("secret"),
+		Balance:  2022,
+	}
+
+	m.EXPECT().AuthUser(testUser.Login, testUser.Password).Return(&testUser, nil)
+
+	h := handler{
+		service:  m,
+		sessions: sessions.NewCookieStore([]byte("secret")),
+	}
+	handler := http.HandlerFunc(h.userLogin)
+
+	b, _ := json.Marshal(map[string]string{
+		"login":    testUser.Login,
+		"password": testUser.Password,
+	})
+
+	req, _ := http.NewRequest(http.MethodPost, "", bytes.NewReader(b))
+
+	recorder := httptest.NewRecorder()
+	handler.ServeHTTP(recorder, req)
+
+	require.Equal(t, recorder.Code, http.StatusOK)
+}
--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -23,3 +23,109 @@ func Test_service_RegisterUser(t *testing.T) {
 	_, err := New(m).RegisterUser("login", "password")
 	require.NoError(t, err)
 }
+
+func Test_service_AuthUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	const (
+		login    string = "login"
+		password string = "password"
+	)
+
+	u := entity.User{
+		Login:    login,
+		Password: utils.Hash(password),
+	}
+
+	m := mocks.NewMockStorage(ctrl)
+	m.EXPECT().GetUserByLogin(login).Return(&u, nil)
+
+	_, err := New(m).AuthUser(login, password)
+	require.NoError(t, err)
+}
+
+func Test_service_CreateOrder(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mocks.NewMockStorage(ctrl)
+
+	testOrder := entity.Order{
+		Number: "422466257468622",
+		UserID: 24,
+	}
+
+	m.EXPECT().SaveUserOrder(testOrder.Number, testOrder.UserID).Return(&testOrder, nil)
+
+	_, err := New(m).CreateOrder(testOrder.Number, testOrder.UserID)
+	require.NoError(t, err)
+}
+
+func Test_service_GetUserOrders(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mocks.NewMockStorage(ctrl)
+
+	testOrders := []*entity.Order{
+		{Number: "6174570601580", UserID: 24},
+		{Number: "422466257468622", UserID: 24},
+		{Number: "2352238521358", UserID: 24},
+	}
+
+	m.EXPECT().SelectOrdersByUserID(24).Return(testOrders, nil)
+
+	actualOrders, err := New(m).GetUserOrders(24)
+	require.NoError(t, err)
+	require.Equal(t, testOrders, actualOrders)
+}
+
+func Test_service_Withdraw(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mocks.NewMockStorage(ctrl)
+
+	testUser := &entity.User{
+		ID:      24,
+		Balance: 2022,
+	}
+
+	m.EXPECT().GetUserByID(testUser.ID).Return(testUser, nil)
+
+	testWithdraw := &entity.Withdraw{
+		UserID: testUser.ID,
+		Sum:    22,
+		Order:  "2352238521358",
+	}
+
+	wantBalance := testUser.Balance - testWithdraw.Sum
+
+	m.EXPECT().SaveWithdraw(testWithdraw).Return(nil)
+	m.EXPECT().UpdateUser(testUser).Return(nil)
+
+	err := New(m).Withdraw(testUser.ID, testWithdraw.Order, testWithdraw.Sum)
+	require.NoError(t, err)
+	require.Equal(t, wantBalance, testUser.Balance)
+}
+
+func Test_service_GetWithdrawals(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mocks.NewMockStorage(ctrl)
+
+	userID := 24
+
+	withdrawals := []*entity.Withdraw{
+		{Order: "6174570601580", UserID: userID, Sum: 11},
+		{Order: "422466257468622", UserID: userID, Sum: 22},
+		{Order: "2352238521358", UserID: userID, Sum: 33},
+	}
+
+	m.EXPECT().SelectWithdrawals(userID).Return(withdrawals, nil)
+
+	_, err := New(m).GetWithdrawals(userID)
+	require.NoError(t, err)
+}
